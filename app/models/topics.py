abb9840af3b6c52c344cc9b2c97d0c894b68276f
Masood <masoodsaaed@gmail.com>
2023-09-08 10:43:14 -0700

    added topic model

---
+++ app/models/topics.py
@@ -0,0 +1,20 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+
+class Topic(db.Model):
+    __tablename__ = 'topics'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String(50), nullable=False)
+
+    posts = db.Relationship("Post", back_populates="topics", cascade="all, delete-orphan")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'topic':self.topic
+        }
