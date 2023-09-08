70c9cf6dd9f3367bb0fc37516df9ca2267d3d222
Masood <masoodsaaed@gmail.com>
2023-09-08 10:42:46 -0700

    updated user.py for db relationships

--- app/models/user.py
+++ app/models/user.py
@@ -14,6 +14,9 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    comments = db.Relationship("Comment", back_populates="users")
+    posts = db.Relationship("Post", back_populates="users")
+
     @property
     def password(self):
         return self.hashed_password
