96b9890d74b916f416ea3666c205538f133d3365
Masood <masoodsaaed@gmail.com>
2023-09-11 17:43:10 -0700

    Added form and basic routes for comment

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models import Comment, db
-from ..forms import postForm
+from ..forms import commentForm
 from sqlalchemy import func
 
 
@@ -14,3 +14,33 @@ def getAllComments():
   """
   comments = Comment.query.all()
   return {'Comments' : [comment.to_dict() for comment in comments]}
+
+@comment_routes.route('/<int:id>')
+def getSingleComment(id):
+  """
+  Gets a single comment details
+  """
+  comment = Comment.query.get(id)
+  if comment:
+    return comment.to_dict()
+  return jsonify({'error': 'Comment not found'}), 404
+
+@comment_routes.route('/new', methods=["POST"])
+def createNewComment():
+  """
+  Creates a new comment
+  """
+  request_data=request.get_json()
+  form = commentForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+  data = form.data
+  if form.validate_on_submit():
+    newComment = Comment(
+      postId = data["postId"],
+      userId = data["userId"],
+      comment = data["comment"]
+    )
+    db.session.add(newComment)
+    db.session.commit()
+    return newComment.to_dict()
+  return {"errors":form.errors}, 401
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,3 +1,4 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .post_forms import postForm
+from .comment_form import commentForm
