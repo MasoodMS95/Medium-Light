7c1adf661cf3a23a28eef5326e897b8f80016913
Masood <masoodsaaed@gmail.com>
2023-09-17 14:20:48 -0700

    Added delete post component

---
+++ react-app/src/components/DeletePostModal/DeletePost.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { useModal } from "../../context/Modal";
+import { useDispatch } from "react-redux";
+import { deletePostThunk, getAllPostsThunk } from "../../store/post";
+import { useHistory } from "react-router-dom";
+
+function DeletePost({id}){
+  const { closeModal } = useModal();
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const deletePost = () => {
+    let res = dispatch(deletePostThunk(id))
+    if(res.error){
+      window.alert("Post not found, so not deleted.")
+      return
+    }
+    closeModal();
+    window.alert("Post deleted!")
+    history.push("/")
+  }
+  return(
+    <React.Fragment>
+      <h2>ARE YOU SURE YOU WANT TO DELETE THE POST?</h2>
+      <div className="postActionConfirmationButtons">
+        <button onClick={closeModal} className="greenButton">CANCEL</button>
+        <button onClick={deletePost} className="redButton">DELETE</button>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default DeletePost;
