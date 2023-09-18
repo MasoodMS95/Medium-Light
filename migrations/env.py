8a4f2d9b6a9bcbff3ff64d21cec6299cd6c630b3
Masood <masoodsaaed@gmail.com>
2023-09-17 17:47:35 -0700

    another another another fix for env.py

--- migrations/env.py
+++ migrations/env.py
@@ -3,11 +3,6 @@ from __future__ import with_statement
 import logging
 from logging.config import fileConfig
 
-from flask import current_app
-config.set_main_option(
-    'sqlalchemy.url',
-    str(current_app.extensions['migrate'].db.get_engine().url).replace('%', '%%'))
-target_metadata = current_app.extensions['migrate'].db.metadata
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -21,6 +16,11 @@ SCHEMA = os.environ.get('SCHEMA')
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+from flask import current_app
+config.set_main_option(
+    'sqlalchemy.url',
+    str(current_app.extensions['migrate'].db.get_engine().url).replace('%', '%%'))
+target_metadata = current_app.extensions['migrate'].db.metadata
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
