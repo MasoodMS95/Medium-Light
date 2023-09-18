9a36b6928c1b24102ef1f27c1cc13c137d145f83
Masood <masoodsaaed@gmail.com>
2023-09-17 17:36:45 -0700

    another env.py update

--- migrations/env.py
+++ migrations/env.py
@@ -4,6 +4,8 @@ import logging
 from logging.config import fileConfig
 
 from flask import current_app
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 
 from alembic import context
 
